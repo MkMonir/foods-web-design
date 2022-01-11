1a38f47df7fd8748eb094d657569f9369742efa7
MkMonir <khanmonir1335@gmail.com>
2022-01-11 22:53:33 +0600

    Food web design using Html - CSS and TailwindCSS

---
+++ dist/script.js
@@ -0,0 +1,17 @@
+// $(".owl-carousel").owlCarousel({
+//   loop: true,
+//   margin: true,
+//   nav: true,
+//   resposive: {
+//     0: {
+//       item: 1,
+//     },
+//   },
+// });
+// $(".one-time").slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true,
+// });
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#49A010",
+        secendary: "#BFBFBF",
+        smtext: "#A1A1A1",
+      },
+      borderRadius: {
+        big: "3rem",
+      },
+      maxWidth: {
+        primary: "600px",
+      },
+      height: {
+        primary: "600px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
